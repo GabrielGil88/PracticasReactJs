39ae8ca707f869de26081b64e33b52c42a07e29f
GabrielGil88 <gaboagil.uxui@gmail.com>
2025-09-15 20:23:08 -0300

    Mostrar texto

---
+++ Practica 04/src/components/MostrarTexto.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+
+export default function MostrarTexto() {
+
+    const [visible, setVisible] = useState(true);
+
+    return (
+        <div style={{textAlign: 'center', marginTop: '20px'}}>
+            <button style={{backgroundColor: "#bb23aa"}} onClick={() => setVisible(!visible)}>
+                {visible ? "Ocultar": "Mostrar"}
+            </button>
+            {visible && <p>Hola, soy un texto</p>}
+        </div>
+    );
+
+}
