3d47636a8644b4a7e04b076d966e27e8a1858af3
GabrielGil88 <gaboagil.uxui@gmail.com>
2025-09-24 20:00:55 -0300

    ejercico DragonBall

--- Practica 05 - En Clase/src/components/DragonBall.jsx
+++ Practica 05 - En Clase/src/components/DragonBall.jsx
@@ -3,24 +3,33 @@ import { useEffect, useState } from "react";
 
 export default function DragonBall() {
 
-    const [personajes, setPersonajes] = useState([])
+    const [personajes, setPersonajes] = useState([]);
+    const [cargando, setCargando] = useState(true);
 
 
     useEffect(() => {
         fetch("https://dragonball-api.com/api/characters")
             .then(res => res.json())
             .then((data) => {
-                setPersonajes(data.items);
+                setPersonajes(data.items || data);
             })
-    })
+            .catch((error) => console.error("Error al cargar personajes", error))
+            .finally(() => setCargando(false));
+    },[]);
+
+    if (cargando) {
+        return <p>Cargando personajes...</p>;
+    }
 
     return (
         <div>
             <div>
                 <h1>Personajes de Dragon Ball</h1>
                 <ul>
-                    {personajes.map((p) => (
-                        <li key={p.id}>{p.name}</li>
+                    {personajes.slice(0,10).map((p) => (
+                        <li key={p.id}>
+                            {p.name} ({p.race})
+                        </li>
                     ))}
                 </ul>
             </div>
